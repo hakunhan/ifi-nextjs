dfb997ce044ae002762534918e4760405cfed870
hakunhan <hakunhan@gmail.com>
2021-08-19 21:09:57 +0700

    fix import

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,38 @@
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
+import NextAuth from "next-auth"
+import Adapters from "next-auth/adapters"
+import 
+import Models from "../../../models"
 
-const options = {
-    
-}
+export default NextAuth({
+  adapter: Adapters.TypeORM.Adapter(
+    // The first argument should be a database connection string or TypeORM config object
+    process.env.MONGODB_URL,
+    // The second argument can be used to pass custom models and schemas
+    {
+      models: {
+        User: Models.User,
+      },
+    }
+  ),
+  session: { jwt: true },
+  callbacks: {
+    async jwt(token, user, account, profile, isNewUser) {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      if (user?.roles) {
+        token.roles = user.roles;
+      }
+      return token;
+    },
+    async session(session, token) {
+      if(token?.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+      if (token?.roles) {
+        session.user.roles = token.roles;
+      }
+      return session;
+    }
+  }
+})
